c71c7ba749f20870b9ea4b17c48d6d4f250b4726
MirMoheen12 <91481488+MirMoheen12@users.noreply.github.com>
2023-03-25 04:59:16 +0500

    Add controllers

---
+++ EbayApi/Controllers/AccountSideController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EbayApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountSideController : ControllerBase
+    {
+    }
+}
